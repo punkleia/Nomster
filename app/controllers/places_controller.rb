c3e16e867ea6361305660116c904c00982cd31af
Michelle Luby <theatrejawa@yahoo.com>
2017-01-19 04:36:39 +0000

    Added edit page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,9 +14,15 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
-def show
-    @place = Place.find(params[:id])
-end
+  def show
+      @place = Place.find(params[:id])
+  end
+
+  def edit
+      @place = Place.find(params[:id])
+  end
+
+  private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
