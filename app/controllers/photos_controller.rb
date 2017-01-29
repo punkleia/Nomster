0af7e49ddf646674524f6059225cc1f95e0da5ff
Michelle Luby <theatrejawa@yahoo.com>
2017-01-29 22:21:28 +0000

    created model, paths, associations, controller, and simple form for user uploaded photos

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,20 @@
+class PhotosController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @place = Place.find(params[:place_id])
+
+    # if @place.user != current_user
+    #    return render text: "Not Allowed", status: :forbidden
+    #else @place.photos.create(photo_params) < incorrect, see below
+
+    @photo = @place.photos.create(photo_params)
+      redirect_to place_path(@place)
+    end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :picture)
+  end
